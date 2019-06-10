154be932a35cdbc7c7c5c7b8c295f40be80e5fbf
Ozar <odilov090996@mail.ru>
2019-06-10 11:43:03 +0700

    Вывод ошибки 404
    Вывод ошибки пользователю при переходе на несуществующую страницу

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -13,7 +13,18 @@ use app\models\ContactForm;
 
 class SiteController extends Controller
 {
-
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
     /**
      * Displays homepage.
      *
